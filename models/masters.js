15d77e2edfd6dfb3cd5e58d28542c2e6f9335683
yntik <eifaurya@gmail.com>
2018-12-18 19:32:16 +0200

    async/await finished

--- models/masters.js
+++ models/masters.js
@@ -1,112 +1,96 @@
-const mysql = require('mysql')
-const mypool = require('../settings/MyPool')
+const mysql = require('mysql');
+const mypool = require('../settings/MyPool');
+const mysql_promise = require('promise-mysql');
+
 
 const master = {
 
 
-    getFreeMaster: ({query}) => {
-        return mypool.getCon()
-            .then((con) => {
-                var start = new Date(String(query.datetime));
-                var end = new Date(String(query.datetime));
-                end.setHours(end.getHours() + Number(query.size))
+    getFreeMaster: async ({query}) => {
+        const con = await mypool.getCon();
 
-                var sql = '';
-                if (query.option === 'new') {
-                    console.log('free master for new order');
-                    sql = 'SELECT masters.id, masters.name, masters.surname, masters.idcity, masters.rating, cities.city\n'
-                        + "FROM masters\n"
-                        + "LEFT JOIN cities ON masters.idcity = cities.id\n"
-                        + "WHERE cities.id = " + mysql.escape(decodeURI(query.city)) + "\n"
-                        + "AND masters.id NOT IN (" + "\n"
-                        + "SELECT idmaster FROM orders WHERE start <= " + mysql.escape(start) + " AND " + mysql.escape(start) + " <= end" + "\n"
-                        + "OR start <= " + mysql.escape(end) + " AND " + mysql.escape(end) + " <= end )";
-                }
-                else {
-                    sql = 'SELECT masters.id, masters.name, masters.surname, masters.idcity, masters.rating, cities.city\n '
-                        + "FROM masters\n"
-                        + "LEFT JOIN cities ON masters.idcity = cities.id\n"
-                        + "WHERE cities.id = " + mysql.escape(decodeURI(query.city)) + "\n"
-                        + "AND masters.id NOT IN (" + "\n"
-                        + "SELECT idmaster FROM orders WHERE (start <= " + mysql.escape(start) + " AND " + mysql.escape(start) + " <= end" + "\n"
-                        + "OR start <= " + mysql.escape(end) + " AND " + mysql.escape(end) + " <= end) AND NOT orders.id = " + mysql.escape(Number(query.option)) + ")";
-                }
+        const start = new Date(String(query.datetime));
+        const end = new Date(String(query.datetime));
+        end.setHours(end.getHours() + Number(query.size))
 
-                return new Promise((resolve, reject) => {
-                    con.query(sql, function (err, result) {
-                        if (err) {
-                            reject(err);
-                            return;
-                        }
-                        con.release();
-                        resolve(result);
-                    })
-                });
-            });
+        var sql = '';
+        if (query.option === 'new') {
+            console.log('free master for new order');
+            sql = 'SELECT masters.id, masters.name, masters.surname, masters.idcity, masters.rating, cities.city\n'
+                + "FROM masters\n"
+                + "LEFT JOIN cities ON masters.idcity = cities.id\n"
+                + "WHERE cities.id = " + mysql.escape(decodeURI(query.city)) + "\n"
+                + "AND masters.id NOT IN (" + "\n"
+                + "SELECT idmaster FROM orders WHERE start <= " + mysql.escape(start) + " AND " + mysql.escape(start) + " <= end" + "\n"
+                + "OR start <= " + mysql.escape(end) + " AND " + mysql.escape(end) + " <= end )";
+        }
+        else {
+            sql = 'SELECT masters.id, masters.name, masters.surname, masters.idcity, masters.rating, cities.city\n '
+                + "FROM masters\n"
+                + "LEFT JOIN cities ON masters.idcity = cities.id\n"
+                + "WHERE cities.id = " + mysql.escape(decodeURI(query.city)) + "\n"
+                + "AND masters.id NOT IN (" + "\n"
+                + "SELECT idmaster FROM orders WHERE (start <= " + mysql.escape(start) + " AND " + mysql.escape(start) + " <= end" + "\n"
+                + "OR start <= " + mysql.escape(end) + " AND " + mysql.escape(end) + " <= end) AND NOT orders.id = " + mysql.escape(Number(query.option)) + ")";
+        }
+
+        const result = await con.query(sql);
+        con.release();
+        return result;
     },
 
-    get: () => {
-        return mypool.getCon()
-            .then((con) => {
-                var sql = 'SELECT masters.id, masters.name, masters.surname, masters.rating, masters.idcity, cities.city\n '
-                    + "FROM masters\n"
-                    + "LEFT JOIN cities ON masters.idcity = cities.id\n"
-                    + "ORDER BY masters.rating DESC";
+    get: async () => {
+        const con = await mypool.getCon();
 
-                return new Promise((resolve, reject) => {
-                    con.query(sql, function (err, result) {
-                        if (err) {
-                            reject(err);
-                            return;
-                        }
-                        con.release();
-                        resolve(result);
-                    })
-                });
-            });
+        const sql = 'SELECT masters.id, masters.name, masters.surname, masters.rating, masters.idcity, cities.city\n '
+            + "FROM masters\n"
+            + "LEFT JOIN cities ON masters.idcity = cities.id\n"
+            + "ORDER BY masters.rating DESC";
+
+        const result = await con.query(sql);
+        con.release();
+        return result;
+
+        // return new Promise((resolve, reject) => {
+        //     con.query(sql, function (err, result) {
+        //         if (err) {
+        //             reject(err);
+        //             return;
+        //         }
+        //         con.release();
+        //         resolve(result);
+        //     })
+        // });
+
     },
 
-    create: ({body}) => {
-        return mypool.getCon()
-            .then(con => {
-                var sql = "INSERT INTO masters (name, surname, rating, idcity) VALUES ("
-                    + mysql.escape(body.name) + ','
-                    + mysql.escape(body.surname) + ','
-                    + mysql.escape(body.rating) + ','
-                    + mysql.escape(body.city) + ")";
+    create: async ({body}) => {
+        const con = await mypool.getCon();
 
-                return new Promise((resolve, reject) => {
-                    con.query(sql, (err, result) => {
-                        if (err) {
-                            return reject(err);
-                        }
-                        con.release();
-                        resolve(result);
-                    })
-                });
-            });
+        const sql = "INSERT INTO masters (name, surname, rating, idcity) VALUES ("
+            + mysql.escape(body.name) + ','
+            + mysql.escape(body.surname) + ','
+            + mysql.escape(body.rating) + ','
+            + mysql.escape(body.city) + ")";
+
+        const result = await con.query(sql);
+        con.release();
+        return result;
+
     },
 
-    edit: ({body}) => {
-        return mypool.getCon()
-            .then(con => {
-                var sql = 'UPDATE masters SET name = ?, surname = ?, rating = ?, idcity = ? WHERE id = ?';
-                return new Promise((resolve, reject) => {
-                    con.query(sql, [
-                        body.name,
-                        body.surname,
-                        body.rating,
-                        body.city,
-                        body.id
-                    ], function (err, result) {
-                        if (err) {
-                            return reject(err);
-                        }
-                        con.release();
-                        resolve(result);
-                    })
-                });
-            });
+    edit: async ({body}) => {
+        const con = await mypool.getCon();
+        const sql = 'UPDATE masters SET name = ?, surname = ?, rating = ?, idcity = ? WHERE id = ?';
+        const result = await con.query(sql, [
+            body.name,
+            body.surname,
+            body.rating,
+            body.city,
+            body.id
+        ]);
+        con.release();
+        return result;
     }
 
 };
--- settings/MyPool.js
+++ settings/MyPool.js
@@ -1,25 +1,36 @@
-var MyPool = require('mysql')
-var config = require('./MYSQL_OPTION')
+var MyPool = require('mysql');
+var config = require('./MYSQL_OPTION');
 
-var pool = MyPool.createPool(config.POOL_OPTION)
+// var pool = MyPool.createPool(config.POOL_OPTION);
+const mysql_promise = require('promise-mysql');
+const pool = mysql_promise.createPool(config.POOL_OPTION);
+
 
 let MySQL = {
 
 
-    getCon: () => {
-        return new Promise((resolve, reject) => {
-            pool.getConnection((err, con) => {
-                if (err) {
-                    console.log(err)
-                    return reject(err)
-                }
-                resolve(con)
-            })
-        })
-
+    // getCon: () => {
+    //     return new Promise((resolve, reject) => {
+    //         pool.getConnection((err, con) => {
+    //             if (err) {
+    //                 console.log(err)
+    //                 return reject(err)
+    //             }
+    //             resolve(con)
+    //         })
+    //     })
+    //
+    //
+    // }
+    getCon: () =>{
+        try {
+            return pool.getConnection();
+        } catch (err) {
+            throw new Error(err);
+        }
 
     }
-}
+};
 
 
 module.exports = MySQL
